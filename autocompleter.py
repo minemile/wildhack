9546cf85590b8eb8f62cb113da5505acc4cb7788
Stepan Evstifeev <minemile69@gmail.com>
2021-11-13 21:15:59 +0300

    Added spell checker

--- autocompleter.py
+++ autocompleter.py
@@ -1,4 +1,5 @@
 from tries import Trie
+import statistics
 
 
 class AutoCompleterBaseline:
@@ -50,24 +51,80 @@ class AutoCompleteBySuffix:
 
 
 class AutoCompleteByInvertedIndex(AutoCompleteBySuffix):
-    def __init__(self, inverted_index, trie, first_prefix=None, max_candidates=20):
+    def __init__(
+        self,
+        inverted_index,
+        trie,
+        spell_checker,
+        queries_score_thr=1, # zero for no fix
+        first_prefix=None,
+        max_candidates=20,
+    ):
         self.max_candidates = max_candidates
         self.inverted_index = inverted_index
         self.first_prefix = first_prefix
         self.trie = trie
+        self.spell_checker = spell_checker
+        self.queries_score_thr = queries_score_thr
+
+    def preprocess_query(self, query):
+        query = query.lower()
+        splitted_query = query.split()
+        for indx, word in enumerate(splitted_query):
+            splitted_query[indx] = word.strip(" .,-")
+        return splitted_query
 
     def query(self, query, max_n=None):
-        splitted_query = query.split()
+        splitted_query = self.preprocess_query(query)
         query_candidates_all = []
         for word in splitted_query:
             candidates = self.get_candidates(word)
-
             candidates_queries = self.get_queries_from_inverted_index(candidates)
+            candidate_queries_score = self.calculate_score_for_candidates_queries(
+                candidates_queries
+            )
+            print(f"Original query score: {candidate_queries_score}")
+            # try to fix spelling
+            if candidate_queries_score <= self.queries_score_thr:
+                (
+                    fixed_word_candidates,
+                    fixed_word_score,
+                ) = self.fix_word_and_get_candidates(word)
+                if fixed_word_score > candidate_queries_score:
+                    candidates_queries = fixed_word_candidates
+
             query_candidates_all.append(candidates_queries)
         result = self.intersect_candidates(query_candidates_all)
         result = self.prepare_output(result, query, max_n)
         return result
-    
+
+    def fix_word_and_get_candidates(self, word):
+        fixed_words = self.spell_checker.GetCandidates([word], 0)
+        fixed_word = fixed_words[0]
+        if fixed_word == word:
+            fixed_word = fixed_words[1]
+        print(f"Fixed word {word} -> {fixed_word}")
+        candidates_for_fixed_word = self.get_candidates(fixed_word)
+        candidates_queries_for_fixed_word = self.get_queries_from_inverted_index(
+            candidates_for_fixed_word
+        )
+        candidate_queries_score_for_fixed_word = self.calculate_score_for_candidates_queries(
+            candidates_queries_for_fixed_word
+        )
+        return candidates_queries_for_fixed_word, candidate_queries_score_for_fixed_word
+
+    def calculate_score_for_candidates_queries(self, candidates_queries):
+        if len(candidates_queries) == 0:
+            return 0
+        scores = []
+        for candidate_query in candidates_queries:
+            popularity = candidate_query.popularity
+            if popularity > 1:
+                scores.append(candidate_query.popularity)
+        if len(scores) == 0:
+            return 0
+        return statistics.mean(scores)
+
     def get_candidates(self, word):
         candidates = self.trie.query(word)
         candidates = sorted(candidates, key=lambda x: x[1], reverse=True)
@@ -76,6 +133,7 @@ class AutoCompleteByInvertedIndex(AutoCompleteBySuffix):
 
     def prepare_output(self, result, original_query, max_n=None):
         sorted_result = sorted(result, key=lambda x: x.popularity, reverse=True)
+        sorted_result = self.delete_duplicates(sorted_result)
         if self.first_prefix is not None:
             return self.make_result_with_first_prefix(
                 original_query, sorted_result, max_n
@@ -83,6 +141,9 @@ class AutoCompleteByInvertedIndex(AutoCompleteBySuffix):
         if max_n is not None:
             sorted_result = sorted_result[:max_n]
         return sorted_result
+
+    # def delete_duplicates(self, sorted_result):
+        # 
 
     def make_result_with_first_prefix(self, original_query, result, max_n):
         prefix_list = []
